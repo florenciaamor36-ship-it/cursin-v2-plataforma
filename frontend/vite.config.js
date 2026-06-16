8b6e123a5a543f03150ce0a15af789b2e074fb77
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-06-16 13:31:57 -0300

    fix: add base path to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cursin-v2-plataforma/",
   plugins: [
     react(),
     VitePWA({ 
