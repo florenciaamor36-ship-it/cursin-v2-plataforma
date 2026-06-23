ecbf323d73372bb0762018b38f0402a68065d9df
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-06-23 13:40:58 -0400

    ci: remover base path para forzar despliegue correcto

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Cursify',
+        short_name: 'coursify',
+        theme_color: '#191e24'
+      }, 
+    })
+  ],
+})
